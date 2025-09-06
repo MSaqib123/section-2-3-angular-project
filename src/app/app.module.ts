a899c19b35bae9c8b91d194e86e37026097334e8
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-06 13:52:10 +0500

    7. A First Summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,50 @@
 //===================================
-// ======= Class_3_to_5 =============
+// ======= Class_3_to_6 =============
 //===================================
-//#region   Class_3_to_5
+//#region   Class_3_to_6
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { HeaderComponent } from './Class_3_to_5/header/header.component';
+// import { UserComponent } from './Class_3_to_5/user/user.component';
+// import { TasksComponent } from './Class_3_to_5/tasks/tasks.component';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   bootstrap: [AppComponent],
+//   //==== standalon component ====
+//   imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+// })
+// export class AppModule {}
+
+// //injection tree
+// //this error comes because  when we work with module we use
+// // BrowserModule
+
+//#endregion
+
+//
+//
+//
+//
+//
+//===================================
+// ======= Class_7_ =============
+//===================================
+//#region   Class_7_
+
+// ************************
+// A First Summary
+// 1. Non-standalon component undestanding
+
+// Note
+// we know now we can add    non-standalon component in  modules   and standAlon component     to  app.module
+// but we can also add  modules in    standAlon components  also   Example : (NewTask -> formModule)
+// ************************
+
+// this
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,22 +56,8 @@ import { TasksComponent } from './Class_3_to_5/tasks/tasks.component';
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  //==== standalon component ====
   imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppModule {}
 
-//injection tree
-//this error comes because  when we work with module we use
-// BrowserModule
-
 //#endregion
-
-//
-//
-//
-//
-//
-//===================================
-// ======= Class_6 =============
-//===================================

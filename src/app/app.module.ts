603abe600dd67328c4803044353b5b5fc8448bb7
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-06 13:53:33 +0500

    updating the name 7. A First Summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,9 +49,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Class_3_to_5/header/header.component';
-import { UserComponent } from './Class_3_to_5/user/user.component';
-import { TasksComponent } from './Class_3_to_5/tasks/tasks.component';
+import { HeaderComponent } from './Class_3_to_6/header/header.component';
+import { UserComponent } from './Class_3_to_6/user/user.component';
+import { TasksComponent } from './Class_3_to_6/tasks/tasks.component';
 
 @NgModule({
   declarations: [AppComponent],
